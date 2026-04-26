db888858034c5e8f16369e675cf9585644f48b22
restandrootholistichealth <restandrootholistichealth@gmail.com>
2026-04-26 12:14:17 -0400

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'rr-scanner-v1';
+const ASSETS = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
